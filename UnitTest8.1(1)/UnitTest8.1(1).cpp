23010b3c95d11d3604ddb6e10275f5693ff9c532
yrii <yural1rn@gmail.com>
2022-11-25 10:43:55 +0200

    laba8.1(1)

---
+++ UnitTest8.1(1)/UnitTest8.1(1).cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba8.1(1)/laba8.1(1).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest811
+{
+	TEST_CLASS(UnitTest811)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[101];
+			char* dest1 = new char[151];
+			dest1[0] = '\0';
+			char* dest2;
+			dest2 = Change(dest1, str, dest1, 0);
+		}
+	};
+}
